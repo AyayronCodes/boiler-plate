c526656095073b1a854b54ab2d074464a982252a
Aaron Han <aaron@ayayron.com>
2022-05-25 15:33:29 -0400

    protect private info

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser");
+const config = require("./config/key");
+const { User } = require("./models/User");
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+// application/json
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://junhee:12345@boilerplate.rtyug.mongodb.net/?retryWrites=true&w=majority"
-  )
+  .connect(config.mongoURI)
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello world!"));
+
+app.post("/register", (req, res) => {
+  // bring in registration info and store in db
+  const user = new User(req.body);
+
+  user.save((err, doc) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
